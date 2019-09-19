c4e10ae42710c6eb09001eee2a75acfc4a8e5024
ahanjaya <hanjaya_mandala@yahoo.com>
2019-09-19 22:41:06 +0800

    OPC_Upper_Body_CW_19092019
    cross arm_v1

--- PIONEER-ROBOT/pioneer_main/scripts/workspace_l2pc.py
+++ PIONEER-ROBOT/pioneer_main/scripts/workspace_l2pc.py
@@ -22,6 +22,7 @@ class Workspace:
         self.lidar_finish = False
         # self.point_clouds = np.empty((0,3))
         self.point_clouds = []
+        self.pcl_intens   = []
 
         ## Subscriber
         rospy.Subscriber('/robotis/sensor/assembled_scan', PointCloud2, self.point_cloud2_callback)
@@ -39,9 +40,11 @@ class Workspace:
         points[:,1] = pc['y']
         points[:,2] = pc['z']
 
-        # print(len(points))
+        intensities = pc['intensities']
+        # print(pc.dtype.names) # ('x', 'y', 'z', 'intensities', 'index')
+
         self.point_clouds.append(points)
-        # self.point_clouds = np.append(self.point_clouds, points, axis=0)
+        self.pcl_intens.append(intensities)
 
     def lidar_turn_callback(self, msg):
         if msg.data == "end":
@@ -50,15 +53,19 @@ class Workspace:
             self.plot_point_cloud()
 
     def plot_point_cloud(self):
-        sleep(4)
-        data = self.point_clouds[-1]
-        rospy.loginfo("[PW] Length point cloud : {}".format(data.shape))
-        v = pptk.viewer(data)
+        sleep(8)
+        pcl_data    = self.point_clouds[-1]
+        intens_data = self.pcl_intens[-1]
 
-        color = data.copy()
-        color[:,:] = [255, 0, 0]
+        rospy.loginfo("[PW] Length point cloud : {}".format(pcl_data.shape))
+        visual_ptk = pptk.viewer(pcl_data)
+        visual_ptk.attributes(intens_data)
 
-        v.attributes(color)
+        # v.attributes(points[['r', 'g', 'b']] / 255., points['i'])
+        # color      = pcl_data.copy()
+        # color[:,:] = [255, 0, 0]
+        # visual_ptk.attributes(color)
+        # visual_ptk.set(point_size=0.001)
 
     def run(self):
         motion = self.motion
